f556047f652f967c14dca13b237c80b3db9e1c50
mrVeles <mrveles@icloud.com>
2014-05-20 22:29:44 +0300

    Responsive

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,8 +1,5 @@
 var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
-console.log(iOS);
-if(iOS == true) {
-	var link = document.createElement('link');
-	link.setAttribute('rel', 'stylesheet');
-	link.setAttribute('href', 'css/ios.css');
-	document.getElementsByTagName('head')['0'].appendChild(link);
+if(iOS == false) {
+	var video = document.getElementsByTagName('video')['0'];
+	video.setAttribute('poster', 'img/poster.svg');
 }
